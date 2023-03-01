f892bad609434c7f351a77e70f4a77369749db04
InMemoriam7th <62008705+InMemoriam7th@users.noreply.github.com>
2023-03-01 01:58:48 +0100

    Ajoutez des fichiers projet.

---
+++ EcoScore/Controleur/Controleur.cs
@@ -0,0 +1,36 @@
+﻿using EcoScore.Modele;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace EcoScore
+{
+    public class Controleur
+    {
+        private static Modele unModele = new Modele("localhost", "eco_score", "root", "");
+
+        public static void InsertClient(Client unClient)
+        {
+            unModele.InsertClient(unClient);
+        }
+
+        public static void UpdateClient(Client unClient)
+        {
+            unModele.UpdateClient(unClient);
+        }
+
+        public static Client SelectWhereClient(int idClient)
+        {
+            return unModele.SelectWhereClient(idClient);
+        }
+        public static List<Client> SelectAllClient()
+        {
+            return unModele.SelectAllClient();
+        }
+
+
+    }
+}
